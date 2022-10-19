c39afbce13a667e45b7226aec4274a8cebacca81
ssirroon <ssirroon@olin.edu>
2022-10-19 12:55:52 -0400

    Uncommenting glider to enable when N=8

--- labs/01_game_of_life/hdl/main.sv
+++ labs/01_game_of_life/hdl/main.sv
@@ -95,7 +95,7 @@ always_comb begin
     bordered_cells_0 = `INIT_5x5_PERIOD2_BLINKER;
   end else if (N == 8) begin
     // Last implementation is what counts.
-    // bordered_cells_0 = `INIT_8x8_GLIDER;
+    bordered_cells_0 = `INIT_8x8_GLIDER;
     // bordered_cells_0 = `INIT_8x8_ALTERNATING;     
   end else if (N==15) begin
     bordered_cells_0 = `INIT_13x13_PULSAR;
